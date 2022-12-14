de08480277f9be417a6b0d51acef624ea53eb461
Hossam Ragab <hossam.alasklany@gmail.com>
2022-12-14 13:27:33 +0200

    header done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocial'
 
 function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <div className='container header__container'>
+                <h5>Hellow I'm</h5>
+                <h1>Hoso</h1>
+                <h5 className='text-light'>front end developer</h5>
+                
+                <CTA />
+                <HeaderSocial />
+
+                <div className='me'>
+                    <img src={ME} alt="me" />
+                </div>
+
+                <a href='#contact' className='scroll__down'>Scroll Down</a>
+            </div>
+        </header>
     )
 }
 
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from 'react-icons/fa'
+import { FaDribbble } from 'react-icons/fa'
+
+function HeaderSocial() {
+    return (
+        <div className='header__social'>
+            <a href='https://linkedin.com' target="_blank" rel="noreferrer">
+                <FaLinkedin />
+            </a>
+            <a href='https://github.com' target="_blank" rel="noreferrer">
+                <FaGithub />
+            </a>
+            <a href='https://dribble.com' target="_blank" rel="noreferrer">
+                <FaDribbble />
+            </a>
+        </div>
+    )
+}
+
+export default HeaderSocial
